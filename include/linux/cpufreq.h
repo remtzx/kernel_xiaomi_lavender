874ded03129f30f8a7ff2b7a24042af393337364
sohamxda7 <sensoham135@gmail.com>
2020-02-15 01:31:23 +0530

    add blu_active

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -554,6 +554,9 @@ extern struct cpufreq_governor cpufreq_gov_interactivex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESSV5)
 extern struct cpufreq_governor cpufreq_gov_darknessv5;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darknessv5)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
