885574f61114170070bf0dc3d6b2d413edf4d746
sohamxda7 <sensoham135@gmail.com>
2020-03-02 12:35:22 +0530

    Revert "nightmare governor added"
    This reverts commit 53cf353ab668ad26a32652bef724d48ff26132fb.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -560,12 +560,6 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
-extern struct cpufreq_governor cpufreq_gov_alucard;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
-extern struct cpufreq_governor cpufreq_gov_nightmare;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
